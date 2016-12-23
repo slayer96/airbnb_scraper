933badaebf8fb8a918df69b4b03f7b4a52bbb13a
slayer96 <myhailo.mykytyn.96@gmail.com>
2016-12-23 19:33:26 +0200

    second commit

--- airbnb_spider/items.py
+++ airbnb_spider/items.py
@@ -8,73 +8,79 @@
 import scrapy
 
 
+class ReviewItem(scrapy.Item):
+    review_text = scrapy.Field()
+    review_summary = scrapy.Field()
+    reference = scrapy.Field()
+    text = scrapy.Field()
+
+
 class AirbnbSpiderItem(scrapy.Item):
     availability = scrapy.Field()
-    accuracy_review = scrapy.Field()
+    accuracy_review = scrapy.Field()  #
     accommodates = scrapy.Field()  #
-    air_conditioning = scrapy.Field()
+    air_conditioning = scrapy.Field()  #
     bathrooms = scrapy.Field()  #
     beds = scrapy.Field()  #
-    breakfast = scrapy.Field()
-    buzzer_of_wireless_intercom = scrapy.Field()
-    cable_TV = scrapy.Field()
+    breakfast = scrapy.Field()  #
+    buzzer_of_wireless_intercom = scrapy.Field()  #
+    cable_TV = scrapy.Field()  #
     check_in_time = scrapy.Field()
     cleaning_fee = scrapy.Field()  #
     cancellation_type = scrapy.Field()  #
-    communication_review = scrapy.Field()
-    cleanliness_review = scrapy.Field()
-    check_in_review = scrapy.Field()
-    doorman = scrapy.Field()
-    dryer = scrapy.Field()
-    description = scrapy.Field()
-    essentials = scrapy.Field()
-    extra_people_charge = scrapy.Field()
-    family_or_kid_friendly = scrapy.Field()
-    free_parking_on_premises = scrapy.Field()
-    gym = scrapy.Field()
-    hangers = scrapy.Field()
-    hair_dryer = scrapy.Field()
-    heating = scrapy.Field()
-    house_rules = scrapy.Field()
-    h24_check_in = scrapy.Field()
-    host_name = scrapy.Field()
+    communication_review = scrapy.Field()  #
+    cleanliness_review = scrapy.Field()  #
+    check_in_review = scrapy.Field()  #
+    doorman = scrapy.Field()  #
+    dryer = scrapy.Field()  #
+    description = scrapy.Field()  #
+    essentials = scrapy.Field()   #
+    extra_people_charge = scrapy.Field()  #
+    family_or_kid_friendly = scrapy.Field()  #
+    free_parking_on_premises = scrapy.Field()  #
+    gym = scrapy.Field()  #
+    hangers = scrapy.Field()  #
+    hair_dryer = scrapy.Field()  #
+    heating = scrapy.Field()  #
+    # house_rules = scrapy.Field()
+    h24_check_in = scrapy.Field()  #
+    host_name = scrapy.Field()  #
     host_description = scrapy.Field()
-    indoor_fireplace = scrapy.Field()
-    iron = scrapy.Field()
-    internet = scrapy.Field()
-    kitchen = scrapy.Field()
-    lift = scrapy.Field()
-    location_review = scrapy.Field()
-    laptop_friendly_workspace = scrapy.Field()
+    indoor_fireplace = scrapy.Field()  #
+    iron = scrapy.Field()  #
+    internet = scrapy.Field()  #
+    kitchen = scrapy.Field()  #
+    lift = scrapy.Field()  #
+    location_review = scrapy.Field()  #
+    laptop_friendly_workspace = scrapy.Field()  #
 
     latitude = scrapy.Field()  #
     longitude = scrapy.Field()  #
 
     monthly_discount = scrapy.Field()  #
     name_of_district = scrapy.Field()
-    property_type = scrapy.Field()
-    pets_allowed = scrapy.Field()
-    pool = scrapy.Field()
-    photo_urls = scrapy.Field()
-    private_entrance = scrapy.Field()
-    review_summary = scrapy.Field()
-    review_text = scrapy.Field()
+    property_type = scrapy.Field()  #
+    pets_allowed = scrapy.Field()  #
+    pool = scrapy.Field()  #
+    photo_urls = scrapy.Field()   #
+    private_entrance = scrapy.Field()  #
+
+    reviews = scrapy.Field()
     response_date = scrapy.Field()
     response_time = scrapy.Field()
-    reviews = scrapy.Field()
-    reference = scrapy.Field()
+
     room_type = scrapy.Field()  #
     spa = scrapy.Field()
     smoking_allowed = scrapy.Field()  #
     suitable_for_events = scrapy.Field()  #
-    shampoo = scrapy.Field()
-    TV = scrapy.Field()
-    wireless_internet = scrapy.Field()
-    wheelchair_accessible = scrapy.Field()
-    washer = scrapy.Field()
+    shampoo = scrapy.Field()  #
+    TV = scrapy.Field()  #
+    wireless_internet = scrapy.Field()  #
+    wheelchair_accessible = scrapy.Field()  #
+    washer = scrapy.Field()  #
     weekly_discount = scrapy.Field()  #
     value_review = scrapy.Field()  #
-    url = scrapy.Field()
+    url = scrapy.Field()  #
 
 
 
--- airbnb_spider/spiders/airbnb_spider.py
+++ airbnb_spider/spiders/airbnb_spider.py
@@ -2,8 +2,8 @@ import json
 import logging
 
 import scrapy
-
-from ..items import AirbnbSpiderItem
+from lxml import html
+from ..items import AirbnbSpiderItem, ReviewItem
 
 QUERY = 'Budapest--Hungary'
 
@@ -34,31 +34,71 @@ class BnbSpider(scrapy.Spider):
     def parse_content_listing(self, response):
         item = AirbnbSpiderItem()
 
-        #json_array = response.xpath('//meta[@id="_bootstrap-room_options"]/@content').extract()
-        #logging.info(json_array)
-        # if json_array:
-        #    airbnb_json_all = json.loads(json_array[0])
-        #    airbnb_json = airbnb_json_all['airEventData']
-        #    item['value_review'] = airbnb_json['visible_review_count']
-        #    item['room_type'] = airbnb_json['room_type']
-        #    item['response_time'] = airbnb_json['response_time_shown']
+        json_array = response.xpath('//meta[@id="_bootstrap-room_options"]/@content').extract()
+        amenities = []
+        if json_array:
+            airbnb_json_all = json.loads(json_array[0])
+            airbnb_json = airbnb_json_all['airEventData']
+            amenities = airbnb_json['amenities']
+            item['response_time'] = airbnb_json['response_time_shown']
         item['availability'] = ''
-        item
+        item['accuracy_rating'] = airbnb_json['accuracy_rating']
+        item['air_conditioning'] = 5 in amenities
         item['accommodates'] = response.xpath('//strong[contains(@data-reactid,"Accommodates")]/text()').extract_first();
         item['beds'] = response.xpath('//strong[contains(@data-reactid,"Beds")]/text()').extract_first()
         item['bedrooms'] = response.xpath('//strong[contains(@data-reactid,"Bedrooms")]/text()').extract_first()
-        item['cancellation_type'] = response.xpath('//strong[contains(@data-reactid,"Cancellation")]/text()').extract_first()
+        item['breakfast'] = 16 in amenities
+        item['buzzer_of_wireless_intercom'] = 28 in amenities
+        item['cable_TV'] = 2 in amenities
+        item['check_in_time'] = ''
         item['cleaning_free'] = response.xpath('//strong[contains(@data-reactid,"Cleaning")]/text()').extract_first()
+        item['cancellation_type'] = response.xpath('//strong[contains(@data-reactid,"Cancellation")]/text()').extract_first()
+        item['cleanliness_review'] = airbnb_json['cleanliness_rating']
+        item['check_in_review'] = airbnb_json['cleanliness_rating']
+        item['communication_review'] = airbnb_json['communication_rating']
+        item['doorman'] = 14 in amenities
+        item['dryer'] = 34 in amenities
+        item['description'] = response.xpath('/html/head/meta[@property="og:description"]/@content').extract_first()
+        item['essentials'] = 40 in amenities
         item['extra_people_charge'] = response.xpath('//strong[contains(@data-reactid,"Extra people")]/text()').extract_first()
+        item['family_or_kid_friendly'] = 31 in amenities
+        item['free_parking_on_premises'] = 9 in amenities
+        item['gym'] = 15 in amenities
+        item['hangers'] = 44 in amenities
+        item['hair_dryer'] = 45 in amenities
+        item['heating'] = 30 in amenities
+        item['h24_check_in'] = 43 in amenities
+        item['host_name'] = airbnb_json_all['hostFirstName']
+        item['host_description'] = ''
+        item['indoor_fireplace'] = 27 in amenities
+        item['iron'] = 46 in amenities
+        item['internet'] = 3 in amenities
+        item['kitchen'] = 8 in amenities
+        item['lift'] = 21 in amenities
+        item['location_review'] = response.xpath('/html/head/meta[@property="airbedandbreakfast:locality"]/@content').extract_first()
+        item['laptop_friendly_workspace'] = 47 in amenities
         item['latitude'] = response.xpath('/html/head/meta[@property="airbedandbreakfast:location:latitude"]/@content').extract_first()
         item['longtitude'] = response.xpath('/html/head/meta[@property="airbedandbreakfast:location:longitude"]/@content').extract_first()
         item['monthly_discount'] = response.xpath('//strong[contains(@data-reactid,"Monthly Discount")]/text()').extract_first()
+        item['name_of_district'] = ''
         item['property_type'] = response.xpath('//strong[contains(@data-reactid,"Property type")]/text()').extract_first()
+        item['pets_allowed'] = 12 in amenities
+        item['pool'] = 7 in amenities
+        item['photo_urls'] = [url['thumbnail_url'] for url in airbnb_json_all['photoData']]
+        item['private_entrance'] = 57 in amenities
+        item['response_date'] = ''
+        item['response_time'] = ''
+        item['reviews'] = ''
         item['room_type'] = response.xpath('//strong[contains(@data-reactid,"Room type")]/text()').extract_first()
-        item['smoking_allowed'] = response.xpath('//span[contains(@data-reactid,"smoking")]/text()').extract_first()
-        item['suitable_for_events'] = response.xpath('//span[contains(@data-reactid,"event")]/text()').extract_first()
+        item['spa'] = ''
+        item['smoking_allowed'] = 11 in amenities
+        item['suitable_for_events'] = 32 in amenities
+        item['shampoo'] = 41 in amenities
+        item['TV'] = 1 in amenities
+        item['wireless_internet'] = 4 in amenities
+        item['wheelchair_accessible'] = 6 in amenities
+        item['washer'] = 33 in amenities
         item['weekly_discount'] = response.xpath('//strong[contains(@data-reactid,"Weekly Discount")]/text()').extract_first()
-
         item['url'] = response.url
 
         yield item
@@ -83,3 +123,9 @@ class BnbSpider(scrapy.Spider):
                 # otherwise we can conclude that the page
                 # has results but that there is only one page.
                 return 1
+
+
+Amenities = {
+    '43' : '24_h',
+    '5': 'air_conditioning'
+}
